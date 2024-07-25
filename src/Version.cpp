d3055e681447a62e1524901fb3f1afe627717039
bennsch <megabob5000@gmx.de>
2024-07-25 07:51:33 -0400

    Version name (ProductToken) updated

--- src/Version.cpp
+++ src/Version.cpp
@@ -40,4 +40,4 @@ const char XCSoar_Version[] = VERSION;
 const TCHAR XCSoar_VersionLong[] = _T(VERSION VERSION_SUFFIX);
 const TCHAR XCSoar_VersionString[] = _T(VERSION VERSION_SUFFIX "-" TARGET);
 const TCHAR XCSoar_VersionStringOld[] = _T(TARGET " " VERSION VERSION_SUFFIX);
-const TCHAR XCSoar_ProductToken[] = _T("XCSoar v" VERSION VERSION_SUFFIX "-" TARGET GIT_SUFFIX);
+const TCHAR XCSoar_ProductToken[] = _T("BenCSoar v1.0  " "(" __DATE__ "--" __TIME__ ")");
