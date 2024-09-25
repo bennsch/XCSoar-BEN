0f2db7fbe3979f6368da21cb54a4f99dbeb113bd
bennsch <megabob5000@gmx.de>
2024-09-25 08:59:09 -0400

    Graphics and colors updated (green)

--- src/Look/Colors.hpp
+++ src/Look/Colors.hpp
@@ -6,9 +6,9 @@
 #include "ui/canvas/Color.hpp"
 
 #ifdef XCSOAR_TESTING
-static constexpr Color COLOR_XCSOAR_LIGHT = Color(0xed, 0x90, 0x90);
-static constexpr Color COLOR_XCSOAR = Color(0xd0, 0x17, 0x17);
-static constexpr Color COLOR_XCSOAR_DARK = Color(0x5d, 0x0a, 0x0a);
+static constexpr Color COLOR_XCSOAR_LIGHT = Color(0x2a, 0xcf, 0x1f);
+static constexpr Color COLOR_XCSOAR = Color(0x1e, 0x90, 0x16);
+static constexpr Color COLOR_XCSOAR_DARK = Color(0x15, 0x67, 0x10);
 #else
 static constexpr Color COLOR_XCSOAR_LIGHT = Color(0xaa, 0xc9, 0xe4);
 static constexpr Color COLOR_XCSOAR = Color(0x3f, 0x76, 0xa8);
