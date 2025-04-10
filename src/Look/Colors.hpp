05e6827c053888d84169db3d9a0567e88a2f6299
bennsch <megabob5000@gmx.de>
2025-04-09 20:08:16 -0400

    Color theme updated

--- src/Look/Colors.hpp
+++ src/Look/Colors.hpp
@@ -10,9 +10,9 @@ static constexpr Color COLOR_XCSOAR_LIGHT = Color(0xed, 0x90, 0x90);
 static constexpr Color COLOR_XCSOAR = Color(0xd0, 0x17, 0x17);
 static constexpr Color COLOR_XCSOAR_DARK = Color(0x5d, 0x0a, 0x0a);
 #else
-static constexpr Color COLOR_XCSOAR_LIGHT = Color(0xf7, 0x8a, 0x40);
-static constexpr Color COLOR_XCSOAR = Color(0xf7, 0x7d, 0x2a);
-static constexpr Color COLOR_XCSOAR_DARK = Color(0xb8, 0x5d, 0x1f);
+static constexpr Color COLOR_XCSOAR_LIGHT = Color(0xd6, 0x88, 0xfa);
+static constexpr Color COLOR_XCSOAR = Color(0xbf, 0x45, 0xf7);
+static constexpr Color COLOR_XCSOAR_DARK = Color(0xb0, 0x1a, 0xf5);
 #endif
 
 static constexpr uint8_t ALPHA_OVERLAY = 0xA0;
