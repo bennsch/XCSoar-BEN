519c1d81962648c2f9693af816a2051a577159fa
bennsch <megabob5000@gmx.de>
2025-04-15 20:52:08 -0400

    Battery warning below 30%

--- src/BatteryTimer.hpp
+++ src/BatteryTimer.hpp
@@ -9,7 +9,7 @@ class BatteryTimer {
   // Battery status for SIMULATOR mode
   // 10% reminder, 5% exit, 5 minute reminders on warnings
 
-  static constexpr unsigned BATTERY_WARNING = 10;
+  static constexpr unsigned BATTERY_WARNING = 30;
   static constexpr unsigned BATTERY_EXIT = 5;
   static constexpr auto BATTERY_REMINDER = std::chrono::minutes(5);
 
