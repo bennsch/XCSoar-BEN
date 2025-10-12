b8f45bb03ccb775d6c7b0e4c1bc09955c43ef261
bennsch <megabob5000@gmx.de>
2025-10-12 16:47:15 -0400

    Display two values

--- src/InfoBoxes/Content/Multi.cpp
+++ src/InfoBoxes/Content/Multi.cpp
@@ -5,6 +5,7 @@
 #include "Interface.hpp"
 #include "InfoBoxes/Data.hpp"
 #include "UIGlobals.hpp"
+#include "Formatter/TimeFormatter.hpp"
 
 
 
@@ -18,9 +19,10 @@ InfoBoxContentMultiDummy::Update(InfoBoxData &data) noexcept
     // return;
   // }
 
-  data.SetValueFromAltitude(calculated.altitude_agl);
-  data.SetComment("comment");
-  data.SetCustom(calculated.altitude_agl + 1);
+//   data.SetValueFromAltitude(calculated.altitude_agl);
+//   data.SetComment("comment");
+  static uint64_t change = 1;
+  data.SetCustom(change++);
 }
 
 void
@@ -28,17 +30,34 @@ InfoBoxContentMultiDummy::OnCustomPaint(Canvas &canvas, const PixelRect &rc) noe
 {
   const DerivedInfo &calculated = CommonInterface::Calculated();
   const Look &look = UIGlobals::GetLook();
+  const FlyingState &flyingState = CommonInterface::Calculated().flight;
+
+
+  auto flightTimeSec = std::chrono::duration_cast<std::chrono::seconds>(flyingState.flight_time);
+  StaticString<32> sTime1, sTime2, sText;
+    
+  if (flightTimeSec.count() <= 0) {
+    sTime1 = "---";
+  } else {
+    FormatTimeTwoLines(sTime1.buffer(), sTime2.buffer(), flightTimeSec);
+  }
+  sText = "Hello";
+
 
   canvas.SetTextColor(COLOR_BLACK);
   canvas.Select(look.info_box.title_font);
+  PixelSize szTime1 = canvas.CalcTextSize(sTime1);
+  int xTime1 = std::max(1, (rc.left + rc.right - (int)szTime1.width) / 2);
+  int yTime1 = rc.top;
+  canvas.TextAutoClipped({xTime1, yTime1}, sTime1);
 
-  StaticString<32> text("Hello");
+  canvas.DrawHLine(rc.GetTopLeft().x, rc.GetTopRight().x, yTime1 + szTime1.height, COLOR_BLUE);
 
-  PixelSize tsize = canvas.CalcTextSize(text);
+  canvas.SetTextColor(COLOR_LIGHT_GRAY);
+  canvas.Select(look.info_box.title_font);
+  PixelSize szText = canvas.CalcTextSize(sText);
+  int xText = std::max(1, (rc.left + rc.right - (int)szText.width) / 2);
+  int yText = yTime1 + szTime1.height;
+  canvas.TextAutoClipped({xText, yText}, sText);
 
-  int x = std::max(1, (rc.left + rc.right - (int)tsize.width) / 2);
-  int y = rc.top;
-  canvas.TextAutoClipped({x, y}, text);
-
-  canvas.DrawHLine(rc.GetTopLeft().x, rc.GetTopRight().x, rc.GetCenter().y, COLOR_GREEN);
 }
